3ff597599504b1e03113467c6050ac35e590397a
FlyawayNutria <61271360+FlyawayNutria@users.noreply.github.com>
2026-06-01 18:06:32 +0100

    Create PositionControl.cpp

---
+++ M&J/PositionControl.cpp
@@ -0,0 +1,415 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <TimerInterrupt_Generic.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <step.h>
+#include "secrets.h"
+#include <webpage.h>
+
+#include <WiFi.h>
+#include "esp_wpa2.h"
+#include <WebServer.h>
+#include <HTTPClient.h> //Send IP to google sheet
+#include "calibrate.h" //Calibrate the MPU6050
+
+struct Vec3 { //3x1 vector
+	float x;
+	float y;
+	float z;
+};
+
+//Multiplies a 3x3 matrix by a 3x1 vector
+Vec3 matMul(const float R[3][3], Vec3 v) {
+	Vec3 out;
+	out.x = R[0][0] * v.x + R[0][1] * v.y + R[0][2] * v.z;
+	out.y = R[1][0] * v.x + R[1][1] * v.y + R[1][2] * v.z;
+	out.z = R[2][0] * v.x + R[2][1] * v.y + R[2][2] * v.z;
+	return out;
+}
+
+float R_sensor_to_robot[3][3] = {
+	{ 1.0, 0.0, 0.0 },
+	{ 0.0, 1.0, 0.0 },
+	{ 0.0, 0.0, 1.0 }
+};
+
+// Manually measured gyro bias, in rad/s.
+// These are subtracted from the raw gyro readings.
+float GYRO_BIAS_X = -0.029064;
+float GYRO_BIAS_Y = -0.012810;
+float GYRO_BIAS_Z = -0.022884;
+
+const char* ssid = WIFI_SSID;
+const char* username = WIFI_USER;
+const char* password = WIFI_PASS;
+const char* url = SCRIPT_URL; //Send IP to google sheet
+
+//Given pins
+const int STEPPER1_DIR_PIN  = 16;
+const int STEPPER1_STEP_PIN = 17;
+const int STEPPER2_DIR_PIN  = 4;
+const int STEPPER2_STEP_PIN = 14;
+const int STEPPER_EN_PIN    = 15;
+const int ADC_CS_PIN        = 5;
+const int ADC_SCK_PIN       = 18;
+const int ADC_MISO_PIN      = 19;
+const int ADC_MOSI_PIN      = 23;
+const int TOGGLE_PIN        = 32;
+
+//Interval constants
+const int PRINT_INTERVAL      = 50;
+const int LOOP_INTERVAL       = 10;
+const int STEPPER_INTERVAL_US = 20;
+const int OUTER_LOOP_INTERVAL = 50;
+
+//Filter constants
+const float C = 0.99;
+const float DT = (float)LOOP_INTERVAL / 1000.0;
+
+//Variables for inner loop balancing
+float Kv = 5.0;
+float Kp = 24.0;
+float Ki = 0.00;
+float Kd = 0.6;
+float setpoint = 88.0;
+float tiltx = 0.0;
+float target_accel = 0.0;
+bool robot_active = false;
+
+float error_old = 0.0;
+float error_integral = 0.0;
+float integrated_velocity = 0.0;
+
+const float MAX_INTEGRAL = 50.0;
+const float MAX_ACCEL = 1000.0;
+const float MAX_SPEED = 40.0;
+
+//Make a perfect 90 degree turn (for maze)
+float steering_offset = 0.0;
+const float TURN_SPEED = 3.0; //Max turn speed
+float current_heading = 0.0; //Absolute angle (degrees)
+float target_heading = 0.0; //Desired angle
+bool is_turning = false; //Flag
+float K_YAW = 0.06; //Proportional gain for turning (tune ts) //0.07 and 0.02 works
+float K_DAMP = 0.01; //Derivative gain for turning
+float yawRate = 0.0;
+
+//Dynamic control variables
+float dynamic_tilt = 0.0;
+float velocity_gain = 0.2;
+const float MAX_TILT = 5.0; //5 degrees
+volatile float target_drive_velocity = 0.0; //volatile means one core changes the other core reads, so always fetch latest value from RAM
+float MAX_DRIVE_VELOCITY = 5.0;
+
+//Variables to move a fixed distance
+volatile bool moving = false;
+volatile int32_t start_pos = 0;
+volatile int32_t move_target = 0;
+const int32_t MOVE_DISTANCE = 6400;
+
+//Moving web stuff to a different core, and updating how loop timing is calculated
+TaskHandle_t WebServerTask;
+unsigned long lastMicros = 0;
+
+void uploadIP() { //Send IP to google sheet
+	HTTPClient http;
+	http.begin(url);
+	http.addHeader("Content-Type", "application/json");
+	String payload = "{\"device\":\"balancebot\"," "\"ip\":\"" + WiFi.localIP().toString() + "\"}";
+	int code = http.POST(payload);
+	Serial.printf("Upload result: %d\n", code);
+	http.end();
+}
+
+ESP32Timer ITimer(3);
+Adafruit_MPU6050 mpu;
+WebServer server(80);
+
+void webServerCode(void * parameter) {
+	for(;;) {
+		server.handleClient();
+		vTaskDelay(10 / portTICK_PERIOD_MS);
+	}
+}
+
+step step1(STEPPER_INTERVAL_US, STEPPER1_STEP_PIN, STEPPER1_DIR_PIN);
+step step2(STEPPER_INTERVAL_US, STEPPER2_STEP_PIN, STEPPER2_DIR_PIN);
+
+String getWebPage() { //Pulls the webpage from webpage.h, and replaces placeholders
+	String html = FPSTR(WEBPAGE_HTML);
+
+	html.replace("%KP%", String(Kp));
+	html.replace("%KI%", String(Ki));
+	html.replace("%KD%", String(Kd));
+	html.replace("%SP%", String(setpoint));
+	html.replace("%KV%", String(Kv));
+	html.replace("%TKP%", String(K_YAW));
+	html.replace("%TKD%", String(K_DAMP));
+
+	return html;
+}
+
+bool TimerHandler(void * timerNo) {
+	step1.runStepper();
+	step2.runStepper();
+	return true;
+}
+
+void resetBot() {
+	robot_active = false;
+	digitalWrite(STEPPER_EN_PIN, HIGH);
+	
+	//Inner loop balancing
+	integrated_velocity = 0.0;
+	error_integral = 0.0;
+	error_old = 0.0;
+	target_accel = 0.0;
+
+	//Turning and movement
+	steering_offset = 0.0;
+	target_drive_velocity = 0.0;
+	is_turning = false;
+	yawRate = 0.0;
+	dynamic_tilt = 0.0;
+	moving = false;
+
+	start_pos = (step2.getPosition() - step1.getPosition()) / 2;
+
+	step1.setTargetSpeedRad(0.0);
+	step2.setTargetSpeedRad(0.0);
+}
+
+void setup() {
+	Serial.begin(115200); //Serial monitor rate
+	//Set pinmodes
+	pinMode(TOGGLE_PIN, OUTPUT);
+	pinMode(STEPPER_EN_PIN, OUTPUT);
+	pinMode(ADC_CS_PIN, OUTPUT);
+	digitalWrite(STEPPER_EN_PIN, HIGH); // disabled during setup
+	digitalWrite(ADC_CS_PIN, HIGH);
+
+	SPI.begin(ADC_SCK_PIN, ADC_MISO_PIN, ADC_MOSI_PIN);
+
+	//Connect to IMU
+	if (!mpu.begin()) {
+		Serial.println("Failed to find MPU6050 chip");
+		while (1) delay(10);
+	}
+	Serial.println("MPU6050 Found!");
+	mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
+	mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+	mpu.setFilterBandwidth(MPU6050_BAND_44_HZ);
+
+	//Connect to Imperial-WPA
+	Serial.print("Connecting to WPA2 Enterprise WiFi");
+	WiFi.disconnect(true);
+	WiFi.mode(WIFI_STA);
+	esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)WIFI_USER,strlen(WIFI_USER));
+	esp_wifi_sta_wpa2_ent_set_username((uint8_t *)WIFI_USER,strlen(WIFI_USER));
+	esp_wifi_sta_wpa2_ent_set_password((uint8_t *)WIFI_PASS,strlen(WIFI_PASS));
+	esp_wifi_sta_wpa2_ent_enable();
+	WiFi.begin(WIFI_SSID);
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
+	Serial.println("\n--- WIFI CONNECTED ---");
+	Serial.print("IP Address: http://");
+	Serial.println(WiFi.localIP());
+	uploadIP();
+
+	server.on("/", []() {
+		server.send(200, "text/html", getWebPage());
+	});
+
+	server.on("/data", []() {
+		String json = "{";
+		json += "\"tilt\":" + String(tiltx, 2);
+		json += ",\"set\":" + String(setpoint, 2);
+		json += ",\"active\":" + String(robot_active ? "true" : "false");
+		json += "}";
+		server.send(200, "application/json", json);
+	});
+
+	server.on("/update", []() {
+		if (server.hasArg("p")) Kp = server.arg("p").toFloat();
+		if (server.hasArg("i")) Ki = server.arg("i").toFloat();
+		if (server.hasArg("d")) Kd = server.arg("d").toFloat();
+		if (server.hasArg("t")) setpoint = server.arg("t").toFloat();
+		if (server.hasArg("v")) Kv = server.arg("v").toFloat();
+		if (server.hasArg("tkp")) K_YAW = server.arg("tkp").toFloat();
+		if (server.hasArg("tkd")) K_DAMP = server.arg("tkd").toFloat();
+
+		error_integral = 0.0;
+		Serial.printf("Web Update | Kp: %.2f | Ki: %.2f | Kd: %.2f | Setpoint: %.2f | Kv: %.2f | TKP: %.2f | TKD: %.2f\n", Kp, Ki, Kd, setpoint, Kv, K_YAW, K_DAMP);
+
+		server.send(200, "text/html", getWebPage());
+	});
+
+	server.on("/control", []() {
+		if (server.hasArg("dir")) {
+			String dir = server.arg("dir");
+			Serial.println(dir);
+			if (dir == "F") { if (!moving) { target_drive_velocity = MAX_DRIVE_VELOCITY; steering_offset = 0.0; is_turning = false; move_target = MOVE_DISTANCE; start_pos = (step2.getPosition() - step1.getPosition()) / 2; moving = true; } }
+			else if (dir == "B") { if (!moving) { target_drive_velocity = -MAX_DRIVE_VELOCITY; steering_offset = 0.0; is_turning = false; move_target = MOVE_DISTANCE; start_pos = (step2.getPosition() - step1.getPosition()) / 2; moving = true; } }
+			else if (dir == "L") { target_drive_velocity = 0.0; steering_offset = -TURN_SPEED; is_turning = false; }
+			else if (dir == "R") { target_drive_velocity = 0.0; steering_offset = TURN_SPEED; is_turning = false; }
+			else if (dir == "T1" && !is_turning) { target_heading = current_heading - 90.0; is_turning = true; }//Turn 90 degrees CW
+			else if (dir == "T2" && !is_turning) { target_heading = current_heading + 90.0; is_turning = true; } //Turn 90 degrees ACW
+			else {
+				if (!moving) { target_drive_velocity = 0.0; }
+				if (!is_turning) { steering_offset = 0.0; }
+			} //Do nothing
+		}
+		server.send(200, "text/plain", "OK");
+	});
+
+	server.on("/calibrate", []() {
+		Serial.println("Web gyro calibration");
+		resetBot();
+		calibrateGyro();
+		server.send(200, "text/plain", "Calibration Complete");
+	});
+
+	server.on("/stop", []() {
+		resetBot();
+		Serial.println("Web Stop");
+		server.send(200, "text/html", getWebPage());
+	});
+
+	server.begin();
+
+	if (!ITimer.attachInterruptInterval(STEPPER_INTERVAL_US, TimerHandler)) {
+		Serial.println("Failed to start stepper interrupt");
+		while (1) delay(10);
+	}
+
+	Serial.println("Initialised Interrupt for Stepper");
+
+	step1.setAccelerationRad(2000.0);
+	step2.setAccelerationRad(2000.0);
+
+	digitalWrite(STEPPER_EN_PIN, LOW); // enable driver after setup
+
+	xTaskCreatePinnedToCore (webServerCode, "WebServerTask", 10000, NULL, 1, &WebServerTask, 0); //Pin web server handling to core 0
+}
+
+void loop() {
+	static unsigned long printTimer = 0;
+	static unsigned long loopTimer = 0;
+	static unsigned long outerLoopTimer = 0;
+
+	//Outer loop dynamic control
+	if (millis() - outerLoopTimer >= OUTER_LOOP_INTERVAL) {
+		outerLoopTimer = millis();
+		float drive_velocity = (step1.getSpeedRad() - step2.getSpeedRad()) * 0.5;
+		float velocity_error = target_drive_velocity - drive_velocity;
+		dynamic_tilt = velocity_gain * velocity_error;
+		dynamic_tilt = constrain(dynamic_tilt, -MAX_TILT, MAX_TILT);
+
+		int32_t current_pos = (step2.getPosition() - step1.getPosition()) / 2;
+		if (moving && abs(current_pos - start_pos) >= move_target) {
+			moving = false;
+			target_drive_velocity = 0.0;
+		}
+	}
+
+	//Inner loop balancing
+	//if (millis() > loopTimer) {
+	if (millis() - loopTimer >= LOOP_INTERVAL) {
+		loopTimer = millis();
+		unsigned long currentMicros = micros();
+		float true_DT = (currentMicros - lastMicros) / 1000000.0;
+		lastMicros = currentMicros;
+
+		//loopTimer += LOOP_INTERVAL;
+
+		sensors_event_t a, g, temp;
+		mpu.getEvent(&a, &g, &temp);
+
+		// Raw MPU readings
+		Vec3 accelRaw = { a.acceleration.x, a.acceleration.y, a.acceleration.z };
+		Vec3 gyroRaw = { g.gyro.x - GYRO_BIAS_X, g.gyro.y - GYRO_BIAS_Y,g.gyro.z - GYRO_BIAS_Z };
+		Vec3 accelRobot = matMul(R_sensor_to_robot, accelRaw); //Correct for sensor calibration
+		Vec3 gyroRobot  = matMul(R_sensor_to_robot, gyroRaw);
+
+		// Use corrected robot-frame axes
+		float accel_tilt = atan2(accelRobot.x, accelRobot.z) * 180.0 / PI;
+		float gyro_tilt  = -gyroRobot.y * 180.0 / PI;
+
+		tiltx = (1.0 - C) * accel_tilt + C * (tiltx + gyro_tilt * true_DT); //Complementary filter
+
+		//Track yaw for the 90 degree turn (x is the yaw axis)
+		yawRate = gyroRobot.x * (180.0 / PI);
+		if (abs(yawRate) < 0.5) { //ignore microjitters in turning
+			yawRate = 0.0;
+		}
+		current_heading += yawRate * true_DT;
+		if (is_turning) {
+			float heading_error = target_heading - current_heading;
+			while (heading_error > 180.0) heading_error -= 360.0;
+			while (heading_error < -180.0) heading_error += 360.0;
+			steering_offset = -(K_YAW * heading_error) + (K_DAMP * yawRate); //PD controller
+			steering_offset = constrain(steering_offset, -TURN_SPEED, TURN_SPEED);
+
+			if (abs(heading_error) < 2.0 && abs(yawRate) < 7.0) { //Stop turning if within 2 degree of target and rotation is slow
+				is_turning = false;
+				steering_offset = 0.0;
+				current_heading = target_heading;
+			}
+		}
+
+		float error = (setpoint + dynamic_tilt) - tiltx;
+
+		// Fall safety
+		if (abs(error) > 30.0 && robot_active) {
+			resetBot();
+		}
+
+		// Auto-arm when close to setpoint
+		if (!robot_active) {
+			if (abs(error) < 0.5) {
+			resetBot();
+			robot_active = true; //Overwrite these after the reset
+			digitalWrite(STEPPER_EN_PIN, LOW);
+			error_old = error;
+			lastMicros = micros();
+			Serial.println("Bot armed");
+			} else {
+			step1.setTargetSpeedRad(0.0);
+			step2.setTargetSpeedRad(0.0);
+			}
+		}
+
+		// Balance controller
+		if (robot_active) {
+			error_integral += error * true_DT;
+			error_integral = constrain(error_integral, -MAX_INTEGRAL, MAX_INTEGRAL);
+
+			// Since error = setpoint - tiltx
+			float error_derivative = -gyro_tilt;
+
+			// PID gives acceleration, Kv damps the integrated velocity
+			target_accel = Kp * error + Ki * error_integral + Kd * error_derivative - Kv * integrated_velocity;
+
+			target_accel = constrain(target_accel, -MAX_ACCEL, MAX_ACCEL);
+
+			integrated_velocity += target_accel * true_DT;
+			integrated_velocity = constrain(integrated_velocity, -MAX_SPEED, MAX_SPEED);
+
+			step1.setTargetSpeedRad(-integrated_velocity - steering_offset);
+			step2.setTargetSpeedRad(integrated_velocity - steering_offset);
+
+			error_old = error;
+		}
+	}
+
+	//server.handleClient();
+
+	//Debug print
+	if (millis() > printTimer) {
+		printTimer += PRINT_INTERVAL;
+		//Print something to serial
+	}
+}
